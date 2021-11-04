0d88cd52c40ab02f9617106cd343d522b786d99f
Adair Juneo <adair_juneo@hotmail.com>
2021-11-03 22:00:07 -0300

    Tela de depoimentos e carrocel criados.

--- main.js
+++ main.js
@@ -26,3 +26,12 @@ window.addEventListener('scroll', ()=>{
         shadowHeader.classList.remove('scroll')
     }
 })
+
+const swiper = new Swiper('.swiper-container',{
+    slidesPerView: 1,
+    pagination:{
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
